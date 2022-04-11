26ceeb676e4f7b64bde65df0a9429658156ed8ca
nikolay19292 <nikolay5_92@mail.ru>
2022-04-11 23:18:31 +0300

    task_4

--- task_4/solution.js
+++ task_4/solution.js
@@ -7,7 +7,7 @@ function parseProducts(json) {
     products = data.products;
     return products.data;
 
-    // Аргументом функции является JSON
+    // Аргументом функции является JSON 1233454545665
 
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
